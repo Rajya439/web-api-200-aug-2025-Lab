077bf6b8d98a9e90244643355f3e4f5e25d63f57
Jeffry Gonzalez <jeff@hypertheory.com>
2025-08-25 16:11:17 -0400

    before options and builders

--- src/Demos/ServicesAndOtelSolution/Demos.Api/Home/PersistentHitCounter.cs
+++ src/Demos/ServicesAndOtelSolution/Demos.Api/Home/PersistentHitCounter.cs
@@ -5,7 +5,7 @@ using Marten;
 namespace Demos.Api.Home;
 
 public class PersistentHitCounter(
-
+    Guid id,
     IServiceScopeFactory scopeFactory
     ) : ICountHits
 
@@ -21,7 +21,7 @@ public class PersistentHitCounter(
         using (var scope = scopeFactory.CreateScope())
         {
             using var session = scope.ServiceProvider.GetRequiredService<IDocumentSession>();
-            var id = Guid.Parse("b37bd889-591a-40b0-b97b-00f007866607");
+            
             var hitCount = await session.Query<HitCounterEntity>()
                 .Where(h => h.Id == id)
                 .SingleOrDefaultAsync();
--- src/Demos/ServicesAndOtelSolution/Demos.Api/Program.cs
+++ src/Demos/ServicesAndOtelSolution/Demos.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Demos.Api.Home;
 using Marten;
 
@@ -6,7 +7,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 //builder.Services.AddTransient<HitCounter>(); // create a new one of these EVERY TIME any controller or service needs the HitCounter.
 //builder.Services.AddScoped<HitCounter>(); // Create a new one of these for each request.
-builder.Services.AddSingleton<ICountHits, PersistentHitCounter>(); // Just a single instance - shared across everything (ever request, every usage)
+builder.Services.AddSingleton<ICountHits>(sp =>
+{
+    // this method is still "lazy" it's not going to run this until the ICountHits is injected somewhere
+    var id = Guid.Parse("b37bd889-591a-40b0-b97b-00f007866607");
+    var ssf = sp.GetRequiredService<IServiceScopeFactory>();
+
+    return new PersistentHitCounter(id, ssf);
+
+}); // Just a single instance - shared across everything (ever request, every usage)
+//builder.Services.AddSingleton<ICountHits, PersistentHitCounter>();
+Dictionary<string, decimal> lookedupPrices = new()
+{
+    { "eggs", 2.99M }
+};
+await Task.Delay(300);
+var service = new SomeSlowStartingService(lookedupPrices);
+
+builder.Services.AddSingleton(service); // This registers an "instance" - so eager.
+
 var connectionString = builder.Configuration.GetConnectionString("scratch") ?? throw new Exception("No Connection String");
 
 builder.Services.AddMarten(c =>
@@ -36,3 +55,36 @@ app.Use(async (context, next) =>
 });
 app.MapControllers(); // Look at the request for the method and the path, and direct to the right place.
 app.Run();
+
+
+
+public class SomeSlowStartingService
+{
+    private readonly Dictionary<string, decimal> _prices;
+    public SomeSlowStartingService(Dictionary<string, decimal> prices)
+    {
+        _prices = prices;
+    }
+    public void DoIt()
+    {
+        Console.WriteLine("Doing some slow work.");
+    }
+
+}
+
+
+public class  ClassDataService
+{
+    public ClassDataService(ClassDataServiceOptions options)
+    {
+        // use those options to set up this service.
+    }
+    // use that stuff in here.
+}
+
+public class ClassDataServiceOptions
+{
+    public string ConnectionString { get; private set; } = string.Empty;
+    public int MaxStudents { get; private set; }
+    public int MinStucents { get; private set; }
+};
