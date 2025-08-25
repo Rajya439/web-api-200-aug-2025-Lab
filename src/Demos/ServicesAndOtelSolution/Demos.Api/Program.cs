45778985ac9e463a5f7935ede4c9a590789f12ad
Jeffry Gonzalez <jeff@hypertheory.com>
2025-08-25 18:30:37 -0400

    Fixed Options

--- src/Demos/ServicesAndOtelSolution/Demos.Api/Program.cs
+++ src/Demos/ServicesAndOtelSolution/Demos.Api/Program.cs
@@ -16,8 +16,10 @@ var builder = WebApplication.CreateBuilder(args);
 //    o.MinStudents = 8;
 //    o.MaxStudents = 16;
 //});
-var config = builder.Configuration.GetSection(ClassDataServiceOptions.OptionsName);
-builder.Services.AddSingleton(config!); 
+//var config = builder.Configuration.GetSection(ClassDataServiceOptions.OptionsName);
+var classOptions = new ClassDataServiceOptions();
+builder.Configuration.GetSection(ClassDataServiceOptions.OptionsName).Bind(classOptions);
+builder.Services.AddSingleton(classOptions); 
 
 builder.Services.AddSingleton<ClassDataService>();
 
@@ -25,16 +27,15 @@ builder.Services.AddSingleton<ClassDataService>();
 builder.Services.AddControllers();
 //builder.Services.AddTransient<HitCounter>(); // create a new one of these EVERY TIME any controller or service needs the HitCounter.
 //builder.Services.AddScoped<HitCounter>(); // Create a new one of these for each request.
+
+
+// Just a single instance - shared across everything (ever request, every usage)
 builder.Services.AddSingleton<ICountHits>(sp =>
 {
-    // this method is still "lazy" it's not going to run this until the ICountHits is injected somewhere
     var id = Guid.Parse("b37bd889-591a-40b0-b97b-00f007866607");
     var ssf = sp.GetRequiredService<IServiceScopeFactory>();
-
     return new PersistentHitCounter(id, ssf);
-
-}); // Just a single instance - shared across everything (ever request, every usage)
-//builder.Services.AddSingleton<ICountHits, PersistentHitCounter>();
+});
 Dictionary<string, decimal> lookedupPrices = new()
 {
     { "eggs", 2.99M }
@@ -71,9 +72,10 @@ app.Use(async (context, next) =>
     Console.Write("Response content-type is", mt);
     Console.WriteLine("Sending a Response");
 });
+
 app.MapControllers(); // Look at the request for the method and the path, and direct to the right place.
 app.Run();
-
+    
 
 
 public class SomeSlowStartingService
@@ -93,11 +95,11 @@ public class SomeSlowStartingService
 
 public class  ClassDataService
 {
-    public ClassDataService(IOptions<ClassDataServiceOptions> options)
+    public ClassDataService(ClassDataServiceOptions options)
     {
         // use those options to set up this service.
-        var cs = options.Value;
-        var x = cs.ConnectionString;
+        
+        var x = options.ConnectionString;
     }
     // use that stuff in here.
 }
